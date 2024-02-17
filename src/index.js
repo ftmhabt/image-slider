6d871baba6265b7c314ffee7274663bb459071f5
ftmhabt <ftmh.a78@gmail.com>
2024-02-17 16:15:23 +0330

    Improve transition & Add interval

--- src/index.js
+++ src/index.js
@@ -4,25 +4,35 @@ const right = document.querySelector('.right');
 const left = document.querySelector('.left');
 const images = Array.from(document.querySelectorAll('img'));
 
-right.addEventListener('click', () => {
+
+function toRight() {
     for (let i = 0; i < images.length; i++) {
         if (images[i].classList.contains('show')) {
             images[i].classList.remove('show');
             const next = (i + 1) % images.length;
             images[next].classList.add('show');
-            break; // Exit the loop after finding the first 'show' image
+            break;
         }
     }
-});
+}
 
-left.addEventListener('click', () => {
-    for (let i = 0; i <images.length; i++) {
+function toLeft() {
+    for (let i = 0; i < images.length; i++) {
         let next;
         if (images[i].classList.contains('show')) {
             images[i].classList.remove('show');
-            next = i===0?images.length-1: (i - 1) ;
+            next = i === 0 ? images.length - 1 : (i - 1);
             images[next].classList.add('show');
-            break; // Exit the loop after finding the first 'show' image
+            break;
         }
     }
-});
+}
+
+
+right.addEventListener('click', toRight);
+left.addEventListener('click', toLeft);
+
+setInterval(toRight, 10000);
+
+
+
